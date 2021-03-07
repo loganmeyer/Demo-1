7992d628eba52b2c0b1e90fc1dad3d601a3cbf33
SkunkHeadedPunk <jack.woolery.94@gmail.com>
2021-03-06 19:14:58 -0700

    Finalized computer vision module and calibration data

--- ComputerVision.py
+++ ComputerVision.py
@@ -26,13 +26,15 @@ import math
 
 camera = PiCamera()
 
-MARKER_LENGTH_MM = 200
+MARKER_LENGTH_MM = 100
 MARKER_LENGTH_IN = MARKER_LENGTH_MM / 25.4
-# 3280, 2464
-WIDTH = 3136
-HEIGHT = 2400
+MARKER_LENGTH_IN = 3.8125
 
-KD = np.load('cam_matrices.npz')
+# Max res: 3280 x 2464
+WIDTH = 3264
+HEIGHT = 2464
+
+KD = np.load('cam_matrices_charuco.npz')
 K = KD['k']
 DIST_COEFFS = KD['dist']
 
@@ -45,9 +47,13 @@ S = 1.12 * 10 ** -6
 Fx = F / S / 2
 Fy = F / S / 2
 # Set camera intrinsic matrix
-K = np.array([[Fx, 0, Cx],
-              [0, Fy, Cy],
-              [0, 0, 1]])
+##K = np.array([[Fx, 0, Cx],
+##              [0, Fy, Cy],
+##              [0, 0, 1]])
+##DIST_COEFFS = 0
+
+# Center point of marker with respect to marker
+P_M = np.array([MARKER_LENGTH_IN / 2, -MARKER_LENGTH_IN / 2, 0, 1])
    
    
 
@@ -55,6 +61,7 @@ arucoDict = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_100)
 
 def detect_marker(img):
     angle = []
+    
 ##    # Perform binarization to enhance ability to locate corners correctly
 ##    _, binary_img = cv.threshold(src=img, maxval=255,
 ##                                 type=cv.THRESH_OTSU, thresh=0)
@@ -62,8 +69,8 @@ def detect_marker(img):
 ##    cv.imshow("thresh_otsu", binary_img)
 ##    cv.waitKey(0)
 
-    #binary_img = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH
-    corners, ids, _ = cv.aruco.detectMarkers(image=img, dictionary=arucoDict)
+    corners, ids, rejectedCorners = cv.aruco.detectMarkers(image=img, dictionary=arucoDict, cameraMatrix=K, distCoeff=DIST_COEFFS)                                   
+
     img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
     
     if ids is not None:
@@ -74,73 +81,135 @@ def detect_marker(img):
                                          ids=ids,
                                          borderColor=(0, 0, 255)
                                          )
-            sum_x = 0
-            sum_y = 0
-            for i in range(4):
-                sum_x = sum_x + corners[0][0][i, 0]
-                sum_y = sum_y + corners[0][0][i, 1]
-            ave_x = sum_x / 4
-            ave_y = sum_y / 4
-            #loc.append([ave_x, ave_y])
-         
-        distance, img_marked = get_distance(corners, img)
-        angle = 0
+            
+
+        tag_center_x = get_center_xval(corners)
+        
+        distance, tm_c, img_marked, disp_img = get_distance(corners, img)
+        
+        angle_deg, angle_rad = get_angle(tag_center_x)
+
+        cv.imshow("Marker Detected", disp_img)
+        cv.waitKey(0)
 
     if ids is None:
         print("Marker not detected")
-        angle = 0
+        angle_deg = 0
+        angle_rad = 0
         distance = 0
-        cv.imshow("img no marker", img)
+        
+        scale = 0.5
+        disp_img = cv.resize(img, (int(img.shape[1] * scale),
+                                   int(img.shape[0] * scale)
+                                   ))
+        cv.imshow("No Marker", disp_img)
         cv.waitKey(0)
-    return distance, angle, img
+        
+    return distance, angle_deg, angle_rad, img
+
+def get_center_xval(corners):
+    sum_x = 0
+    sum_y = 0
+    for i in range(4):
+        sum_x = sum_x + corners[0][0][i, 0]
+        sum_y = sum_y + corners[0][0][i, 1]
+    ave_x = sum_x / 4
+    ave_y = sum_y / 4
+    
+    return ave_x
+
+def get_angle(x):
+    rel_x = abs(Cx - x)
+    angle_deg = (FOV / 2) * (rel_x / Cx)
+    
+    if Cx - x < 0:
+        angle_deg = - angle_deg
+        
+    angle_rad = math.pi * angle_deg / 180
+
+    print("angle: ", round(angle_deg, 2), "degrees;    ",
+          round(angle_rad, 2), "radians")
+    return angle_deg, angle_rad
+
+def inversePerspective(rvec, tvec):
+    R, _ = cv.Rodrigues(rvec)
+    R = np.matrix(R).T
+    invTvec = np.dot(-R, np.matrix(tvec))
+    invRvec, _ = cv.Rodrigues(R)
+    return invRvec, invTvec
 
 def get_distance(corners, img):
-    print("k: ", K)
-    print("dist: ", DIST_COEFFS)
+##    print("k: ", K)
+##    print("dist: ", DIST_COEFFS)
     rvecs, tvecs, _ = cv.aruco.estimatePoseSingleMarkers(
         corners,
         markerLength = MARKER_LENGTH_IN,
         cameraMatrix = K,
         distCoeffs = DIST_COEFFS
         )
-    print("rvecs: ", rvecs)
-    print("tvecs: ", tvecs)
-    # Unpack vectors
-    rvec_m_c = rvecs[0]
-    tm_c = tvecs[0]
-    # Use function to draw axis on Aruco marker
+    #print("rvecs: ", rvecs)
+##    print("tvecs: ", tvecs)
+##    # Unpack vectors
+##    rvec_m_c = rvecs[0]
+##    tm_c = tvecs[0]
+##    # Use function to draw axis on Aruco marker
+##    cv.aruco.drawAxis(image=img, cameraMatrix=K, distCoeffs=DIST_COEFFS,
+##                      rvec=rvec_m_c, tvec=tm_c, length=MARKER_LENGTH_IN)
+##
+##    rvec1, tvec1 = rvecs.reshape((3, 1)), tvecs.reshape((3, 1))
+##    #print("rvec1: ", round(rvec1, 2))
+##
+##    # Invert vectors
+##    invRvec, invTvec = inversePerspective(rvec1, tvec1)
+##
+##    #print("invRvec: ", invRvec)
+##    print("invTvec: ", invTvec)
+##
+##    invDistance = math.sqrt(invTvec[0] ** 2 + invTvec[2] ** 2)
+##    print("invDistance: ", round(invDistance, 2))
+##
+##    #invAngle = np.arctan(invTvec[1] / invTvec[0])
+##    invAngle = np.arctan(invTvec[0] / invTvec[2])
+##    invAngle = invAngle[0][0]
+##    invAngle = float(invAngle)
+##
+##    invAngle_deg = invAngle * 180 / math.pi
+##    print("invAngle: ", round(invAngle_deg, 2), "degrees;     ",
+##          round(invAngle, 2), "radians")
+
+    # Unpack translation vector
+    t_vec = tvecs[0][0]
+    print("t_vec: ", t_vec)
     
-    cv.aruco.drawAxis(image=img, cameraMatrix=K, distCoeffs=DIST_COEFFS,
-                      rvec=rvec_m_c, tvec=tm_c, length=MARKER_LENGTH_IN)
+    distance = math.sqrt(t_vec[0] ** 2 + t_vec[2] ** 2)
+    print("distance: ", round(distance, 2), "inches")
+
+    tan_angle_rad = np.arctan(t_vec[0] / t_vec[2])
+    tan_angle_rad = - tan_angle_rad
+    tan_angle_deg = tan_angle_rad * 180 / math.pi
+    print("tan_angle: ", round(tan_angle_deg, 2), "degrees;     ",
+          round(tan_angle_rad, 2), "radians")
 
     scale = 0.5
     disp_img = cv.resize(img, (int(img.shape[1] * scale),
                                int(img.shape[0] * scale)
                                ))
+    
 
-    cv.imshow("img with axis", disp_img)
-    cv.waitKey(0)
+##    cv.imshow("img with axis", disp_img)
+##    cv.waitKey(0)
 
     # Calculate distance using the root of the sum of the squares from the
     # translation vector
-    distance = math.sqrt(tm_c[0][0] ** 2 + tm_c[0][2] ** 2)
-    print("distance: ", distance, "inches")
+##    distance = math.sqrt(tm_c[0][0] ** 2 + tm_c[0][2] ** 2)
+##    print("distance: ", round(distance, 2), "inches")
     
     
-    return distance, img
+    return distance, t_vec, img, disp_img
 
 
 if __name__ == '__main__':
-##    x = input("Enter the number of seconds you'd like to take photos for: ")
-##    # Change user input to int to set time for capture stream
-##    x = int(x)
-##    t_minus_x_seconds = time.time() + x
 
-##    camera.shutter_speed = camera.exposure_speed
-##    camera.exposure_mode = 'off'
-##    g = camera.awb_gains
-##    camera.awb_mode = 'off'
-##    camera.awb_gains = g
     camera.resolution = (WIDTH, HEIGHT)
     #camera.framerate = 15
 
@@ -148,16 +217,13 @@ if __name__ == '__main__':
         #while time.time() < t_minus_x_seconds:
             camera.capture(stream, format="bgr")
             img = stream.array
-            print("img size: ", img.shape)
 
             gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-            distance, angle, img_markers = detect_marker(gray_img)
-
-##            cv.imshow("Markers Stream", img_markers)
-##            cv.waitKey(2)
-##            cv.destroyWindow("Markers, stream")
+            distance, angle_deg, angle_rad, img_mark = detect_marker(gray_img)
+            
             # Truncate the stream to clear for next image capture
             stream.truncate(0)
 
     cv.destroyAllWindows()
+
